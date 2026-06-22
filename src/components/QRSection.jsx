f6afb7a1bafac9574c330935e5634546aafd4955
leon2192 <leon.strokes@gmail.com>
2026-06-21 21:31:04 -0300

    fc

--- src/components/QRSection.jsx
+++ src/components/QRSection.jsx
@@ -2,11 +2,13 @@ import SectionShell from './SectionShell';
 import SectionIntro from './SectionIntro';
 
 function QRSection({ icon, qr }) {
+  const qrImageSrc = '/assets/qr.jpeg';
+
   return (
     <SectionShell className="section-shell--compact">
       <div className="panel panel--narrow qr-panel">
         <SectionIntro icon={icon} title={qr.title} />
-        <img className="qr-image" src={qr.image} alt="Codigo QR del evento" />
+        <img className="qr-image" src={qrImageSrc} alt="Codigo QR del evento" />
         <p className="section-helper">{qr.helperText}</p>
         <a
           className="button button--primary"
